bc5c88db22eda6359484cd4aad5f8548582dcf2e
rajavau <69924288+rajavau@users.noreply.github.com>
2020-08-20 13:29:38 +0600

    Update and rename zeroboy.py to raja.py

--- raja.py
+++ raja.py
@@ -49,16 +49,16 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - Kamal -OFFICAL------•◈•
+|__\~~) •||•RAJA-VAU - RAJPUT---------------•◈•
+|__(-----\  •◈•------HG-Gang--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
+\033[1;96mAuthor  \033[1;93m: \033[1;92mKamalraja
+\033[1;96mInstagram \033[1;93m: \033[1;FlowRajaVau
+\033[1;96mFacebook  \033[1;93m: \033[1; KamalRaja
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
 \033[1;91m======================================="""
 
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
+\033[1;96mAuthor  \033[1;93m: \033[1;92mRajaVau
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRajaVau
+\033[1;96mFacebook  \033[1;93m: \033[1;92m KamalRaja
 \033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "raja"
+CorrectPassword = "vau"
 
 loop = 'true'
 while (loop == 'true'):
